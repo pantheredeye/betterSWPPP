6eaefcc5269c5159be0cc4c4b56fad392d134ce6
PantheRedEye <barrettburnworth@gmail.com>
2024-09-27 20:47:41 -0500

    V8 Upgrade

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,7 @@
 // https://prettier.io/docs/en/options.html
 /** @type {import('prettier').RequiredOptions} */
-module.exports = {
-  trailingComma: 'es5',
+export default {
+trailingComma: 'es5',
   bracketSpacing: true,
   tabWidth: 2,
   semi: false,
