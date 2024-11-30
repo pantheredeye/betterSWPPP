97fdf522e6b5f1e16e22eba84cd4acbb5a4bc0e7
PantheRedEye <barrettburnworth@gmail.com>
2024-11-30 10:44:46 -0600

    signup page testing

---
+++ web/src/pages/SignupPage/SignupPage.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@redwoodjs/testing/web'
+
+import SignupPage from './SignupPage'
+
+
+describe('SignupPage', () => {
+  it('renders successfully', () => {
+    expect(() => {
+      render(<SignupPage />)
+    }).not.toThrow()
+  })
+})
