8e89a99b9d350f0e94b7179456da3d201a72eea9
PantheRedEye <barrettburnworth@gmail.com>
2024-12-01 09:52:47 -0600

    feat: add OrganizationContext

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,13 +8,16 @@ import './scaffold.css'
 import { AuthProvider, useAuth } from './auth'
 
 import './index.css'
+import { OrganizationProvider } from './context/OrganizationContext'
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
       <AuthProvider>
         <RedwoodApolloProvider useAuth={useAuth}>
+          <OrganizationProvider>
           <Routes />
+          </OrganizationProvider>
         </RedwoodApolloProvider>
       </AuthProvider>
     </RedwoodProvider>
