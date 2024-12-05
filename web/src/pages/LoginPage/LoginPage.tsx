f14425db498d96a5f18ac577a5bcc0a62cb55e10
Barrett Burnworth <77902178+pantheredeye@users.noreply.github.com>
2024-12-05 06:52:46 -0600

    chore: update styling and form property

--- web/src/pages/LoginPage/LoginPage.tsx
+++ web/src/pages/LoginPage/LoginPage.tsx
@@ -55,7 +55,7 @@ const LoginPage = () => {
           <img
             className="mx-auto h-10 w-auto"
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="BetterSWPPP"
+            alt="SWPPP-TOP"
           />
           <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
@@ -80,7 +80,6 @@ const LoginPage = () => {
                   <TextField
                     name="email"
                     ref={emailRef}
-                    type="email"
                     autoComplete="email"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
