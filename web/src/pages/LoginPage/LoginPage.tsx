23b613763aba2f0d741bb9c9cd52eb0c9d69ec77
Barrett Burnworth <77902178+pantheredeye@users.noreply.github.com>
2024-12-06 06:43:55 -0600

    logo update

--- web/src/pages/LoginPage/LoginPage.tsx
+++ web/src/pages/LoginPage/LoginPage.tsx
@@ -54,7 +54,7 @@ const LoginPage = () => {
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img
             className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            src="/android-chrome-512x512.png"
             alt="SWPPP-TOP"
           />
           <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
